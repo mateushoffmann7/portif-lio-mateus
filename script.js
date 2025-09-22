394d4ecbe754ac8fa4a1f1c55f5796d63690bbfc
Mettel07 <mateushoffmann0707@gmail.com>
2025-09-22 18:29:57 -0300

    add scrolly mode and adjust background color

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const skill = document.querySelector(".skills em");
 function handleToggleSkills1() {
   if (skill.innerHTML === "Frontend Developer") {
     skill.innerHTML = "React | Typescript | NextJS | JavaScript";
+    skill.style.transition = " all";
   }
 }
 setInterval(handleToggleSkills1, 3000);
@@ -10,6 +11,19 @@ setInterval(handleToggleSkills1, 3000);
 function handleToggleSkills2() {
   if (skill.innerHTML === "React | Typescript | NextJS | JavaScript") {
     skill.innerHTML = "Frontend Developer";
+    skill.style.transition = " all";
   }
 }
 setInterval(handleToggleSkills2, 6000);
+
+const navBar = document.querySelector(".headerSite");
+
+function handleScrollNav() {
+  if (window.scrollY > 0) {
+    navBar.classList.add("headerSiteColor");
+  } else {
+    navBar.classList.remove("headerSiteColor");
+  }
+}
+
+window.addEventListener("scroll", handleScrollNav);
